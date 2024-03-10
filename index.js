3ea727ef917b5a77cebe8d0fde779900d3799880
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-03-10 20:35:24 +0100

    added api

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  let weatherTemperature = document.querySelector("#temperature");
+  weatherTemperature.innerHTML = response.data.temperature.current;
+}
+
+function citySearch(city) {
+  let apiKey = "280o02ba0daf2b414a53ctfe4e6155a2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input-id");
   let cityElement = document.querySelector("#weather-city-id");
   cityElement.innerHTML = searchInput.value;
+  citySearch(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search");
